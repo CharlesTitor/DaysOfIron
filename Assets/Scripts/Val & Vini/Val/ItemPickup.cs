31b6938ccc8a756c63f1bc9639c835eeae5323ca
Nuribeth_Rosales <namvalin@gmail.com>
2026-09-22 10:37:38 -0600

    Corrections
    We aded a scrip to the objects

--- Assets/Scripts/Val & Vini/Val/ItemPickup.cs
+++ Assets/Scripts/Val & Vini/Val/ItemPickup.cs
@@ -5,12 +5,13 @@ public class ItemPickup : MonoBehaviour, IPickable
     public ItemData ItemData;
     [SerializeField] private float _pickupDistance = 5f;
     [SerializeField] private int _quantity = 1;
+    [SerializeField] private Transform _playerPosition;
 
 
     private void OnMouseDown()
     {
         //TODO : Change Camera.main.transform.position to player position
-        if (Vector3.Distance(this.transform.position, Camera.main.transform.position) <= _pickupDistance)
+        if (Vector3.Distance(this.transform.position, _playerPosition.position) <= _pickupDistance)
         {
             PickUp();
         }
